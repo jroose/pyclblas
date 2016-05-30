630f620ef3c40ac5b300904cbef81134a87ee679
Jon Roose <athanaric-devel@gmail.com>
2016-05-30 12:43:15 -0400

    Moved Extension ahead of setup in setup.py

--- setup.py
+++ setup.py
@@ -10,16 +10,17 @@ sys.path.insert(0,"./src")
 #with open("src/pyclblas.py","wb") as fout:
 #    fout.write(gen_output("src/pyclblas_functions.i"))
 
+pyclblas_swig = Extension('_pyclblas_swig', ['src/pyclblas_swig.i'], libraries=['clBLAS'])
 
 setup(
     name='pyclblas',
-    version='0.8.0',
+    version='0.8.1',
     py_modules=['pyclblas_swig', 'pyclblas'],
     description='Python Bindings for the OpenCL BLAS library (clBLAS)',
     author='Jon Roose',
     author_email='jroose@gmail.com',
     url='https://github.com/jroose/pyclblas',
-    ext_modules=[Extension('_pyclblas_swig', ['src/pyclblas_swig.i'], libraries=['clBLAS'])],
+    ext_modules=[pyclblas_swig],
     requires=['numpy','pyopencl'],
     package_dir={'': 'src'},
     license="Apache 2.0",
